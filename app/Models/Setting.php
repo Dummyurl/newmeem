2116326767a6903e10f12c9eecf6f29aa58bb9ce
uusa35 <uusa35@gmail.com>
2018-10-17 11:25:56 +0300

    updates

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -5,6 +5,6 @@ namespace App\Models;
 
 class Setting extends PrimaryModel
 {
-    protected $localeStrings = ['name','address','country'];
+    protected $localeStrings = ['company','address','country'];
     protected $guarded = [''];
 }
