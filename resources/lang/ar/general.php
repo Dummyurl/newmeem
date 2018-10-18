e7317bae2bb60522263ed5a964dc3da7f5b0c833
root <uusa35@gmail.com>
2018-10-18 04:23:25 +0000

    trans

--- resources/lang/ar/general.php
+++ resources/lang/ar/general.php
@@ -1,8 +1,15 @@
 <?php
 
 return array (
+  'information' => 'معلومات',
   'pages' => 'الصفحات',
+  'add_to_cart' => 'اضافة الى السله',
+  'my_wishlist' => 'المفضلة',
+  'home' => 'الرئيسية',
+  'add_to_favorite' => 'ااضافة للمفضل',
   'kwd' => 'د.ك',
   'status' => 'الحالة',
-  'new_arrival' => 'أحد المنتجات'
+  'new_arrival' => 'أحد المنتجات',
+  'aboutus' => 'عن',
+  'best_sale_products' => 'الاكثر مبيعا',
 );
--- resources/lang/en/general.php
+++ resources/lang/en/general.php
@@ -5,4 +5,11 @@ return array (
   'new_arrival' => 'New Arrivals',
   'kwd' => 'KWD',
   'status' => 'Status',
+  'aboutus' => 'about us',
+  'information' => 'Information',
+  'add_to_cart' => 'add to cart',
+  'add_to_favorite' => 'Add to Favorite',
+  'home' => 'home',
+  'best_sale_products' => 'best sale',
+  'my_wishlist' => 'Wishlist',
 );
