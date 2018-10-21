5e6b7b2cbf736e9300445d80151e1e434b51db28
root <uusa35@gmail.com>
2018-10-21 19:04:50 +0000

    trans

--- resources/lang/ar/message.php
+++ resources/lang/ar/message.php
@@ -1,6 +1,9 @@
 <?php
 
 return array (
+  'have_coupon_message' => 'ادخل الكوبون',
+  'shipping_method_message_with_no_aramex' => 'طرقة الدفع',
+  'checkout' => 'عملية الدفع',
   'loading' => 'جاري التحميل ...',
   'no_items_found' => 'لا يوجد نتائح للبحث. يرجى المحاولة مرة أخرى.',
   'copy_right' => 'جميع الحقوق محفوظة',
