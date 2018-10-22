40c1889fe9b42ea0a1a43b45b0b50738e944789a
uusa35 <uusa35@gmail.com>
2018-10-22 14:50:01 +0300

    trans

--- resources/lang/ar/message.php
+++ resources/lang/ar/message.php
@@ -1,13 +1,10 @@
 <?php
 
 return array (
-<<<<<<< HEAD
-=======
   'have_coupon_message' => 'ادخل الكوبون',
   'shipping_method_message_with_no_aramex' => 'طرقة الدفع',
   'checkout' => 'عملية الدفع',
   'loading' => 'جاري التحميل ...',
->>>>>>> 29ece368513027d50e94ef6e6c76bbbf8b4a6474
   'no_items_found' => 'لا يوجد نتائح للبحث. يرجى المحاولة مرة أخرى.',
   'loading' => 'جاري التحميل ...',
   'copy_right' => 'جميع الحقوق محفوظة',
