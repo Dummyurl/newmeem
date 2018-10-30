0ca48a937fd112ba77eb8f2dcbe1aa63eb0a7cdc
uusa35 <uusa35@gmail.com>
2018-10-30 11:54:43 +0300

    updates

--- resources/views/frontend/partials/scripts.blade.php
+++ resources/views/frontend/partials/scripts.blade.php
@@ -5,3 +5,12 @@
 @endif
 <script src="{{ mix('js/app.js') }}"></script>
 <script src="{{ mix('js/jquery.prettyPhoto.min.js') }}"></script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-113550846-1"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-113550846-1');
+</script>
