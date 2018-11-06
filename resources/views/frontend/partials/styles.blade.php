9b36b021bd7d7b2377e67ccbfe4e77dfa70a2f68
uusa35 <uusa35@gmail.com>
2018-11-06 21:27:15 +0300

    updates

--- resources/views/frontend/partials/styles.blade.php
+++ resources/views/frontend/partials/styles.blade.php
@@ -1,5 +1,5 @@
 @if (app()->isLocale('ar'))
-<link rel="stylesheet" href="{{ mix('css/frontend-rtl.css') }}">
+    <link rel="stylesheet" href="{{ mix('css/frontend-rtl.css') }}">
 @else
     <link rel="stylesheet" href="{{ mix('css/frontend.css') }}">
 @endif
