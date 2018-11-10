75525fc63c8fd0203761b5805d7d5b4cc90e4593
root <uusa35@gmail.com>
2018-11-10 13:51:24 +0000

    trans

--- resources/lang/ar/message.php
+++ resources/lang/ar/message.php
@@ -1,10 +1,27 @@
 <?php
 
 return array (
+  'we_received_your_order_order_shall_be_reviewed_thank_your_for_choosing_our_service' => 'تلقينا طلبك وسيتم مراجعته شكرا لك لاختيار خدماتنا',
+  'register' => 'التسجيل',
   'have_coupon_message' => 'ادخل الكوبون',
   'shipping_method_message_with_no_aramex' => 'طرقة الدفع',
+  'you_are_logged_in_as' => 'أنت مسجل دخولك باسم',
   'checkout' => 'عملية الدفع',
+  'register_with_us' => 'سجل معنا',
+  'register_and_save_time' => 'سجل ووفر الوقت',
   'loading' => 'جاري التحميل ...',
+  'no_products' => 'لا يوجد منتجات',
+  'search_not_correct' => 'البحث غير صحيح',
+  'item_added_to_cart' => 'تم اضافة الى عربة التسوق',
+  'item_removed' => 'تمت إزالة العنصر',
+  'cart_destroyed' => 'تم حذف العربة!',
+  'coupon_shall_be_applied' => 'يتم تطبيق القسيمة',
   'no_items_found' => 'لا يوجد نتائح للبحث. يرجى المحاولة مرة أخرى.',
+  'favorite_saved' => 'حفظ المفضلة',
+  'update_success' => 'تم التحديث',
+  'update_error' => 'خطأ تحديث',
+  'delete_success' => 'تم الحذف',
+  'delete_error' => 'خطأ حذف',
+  'page_does_not_exist' => 'الصفحة لا توجد',
   'copy_right' => 'جميع الحقوق محفوظة',
 );
---
+++ resources/lang/en/group.php
@@ -0,0 +1,6 @@
+<?php
+
+return array (
+  'address' => 'Adress',
+  'name' => 'Name',
+);
