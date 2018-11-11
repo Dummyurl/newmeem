498da2612074aca11c14a54d267e359916d8f27e
uusa35 <uusa35@gmail.com>
2018-11-11 08:00:28 +0300

    updates

--- resources/views/frontend/partials/_top_bar_currencies.blade.php
+++ resources/views/frontend/partials/_top_bar_currencies.blade.php
@@ -1,13 +1,13 @@
 <li class="dropdown currency">
     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-        <img class="img-xxs" src="{{ asset(env('THUMBNAIL').session('currency')->country->flag) }}" alt=""/>
+        <img class="img-flag" src="{{ asset(env('THUMBNAIL').session('currency')->country->flag) }}" alt=""/>
         {{ session('currency')->name }}
         <i class="fa fa-angle-down"></i></a>
     <ul role="menu" class="dropdown-menu">
         @foreach($currencies as $currency)
             <li>
                 <a href="{{ route('frontend.currency.change',['currency' => strtolower($currency->currency_symbol_en)]) }}">
-                    <img class="img-xxs" src="{{ asset(env('THUMBNAIL').$currency->country->flag) }}"/>
+                    <img class="img-flag" src="{{ asset(env('THUMBNAIL').$currency->country->flag) }}"/>
                     {{ $currency->name }}
                 </a>
             </li>
--- resources/views/frontend/partials/_top_bar_language.blade.php
+++ resources/views/frontend/partials/_top_bar_language.blade.php
@@ -1,15 +1,15 @@
 <li class="dropdown flags">
-    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><img class="img-xxs" src="{{ app()->isLocale('ar') ? asset('img/flags/kw.png') : asset('images/flags/us.png') }}" alt="{{ app()->getLocale() }}"/> {{ strtoupper(app()->getLocale()) }}<i class="fa fa-angle-down"></i></a>
+    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><img class="img-flag" src="{{ app()->isLocale('ar') ? asset('img/flags/kw.png') : asset('images/flags/us.png') }}" alt="{{ app()->getLocale() }}"/> {{ strtoupper(app()->getLocale()) }}<i class="fa fa-angle-down"></i></a>
     <ul role="menu" class="dropdown-menu">
         <li>
             <a href="{{ route('frontend.language.change',['locale' => 'ar']) }}">
-                <img class="img-xxs" src="{{ asset('img/flags/kw.png') }}" alt="{{ config('app.name') }}"/>
+                <img class="img-flag" src="{{ asset('img/flags/kw.png') }}" alt="{{ config('app.name') }}"/>
                 {{ trans('general.arabic') }}
             </a>
         </li>
         <li>
             <a href="{{ route('frontend.language.change',['locale' => 'en']) }}">
-                <img class="img-xxs" src="{{ asset('images/flags/us.png') }}" alt="{{ config('app.name') }}"/>
+                <img class="img-flag" src="{{ asset('images/flags/us.png') }}" alt="{{ config('app.name') }}"/>
                 {{ trans('general.english') }}
             </a>
         </li>
