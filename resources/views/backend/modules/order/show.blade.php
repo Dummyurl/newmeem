15eee736f9048c3babee5833fa39d4bf505116fa
uusa35 <uusa35@gmail.com>
2018-11-12 17:25:30 +0300

    updates

--- resources/views/backend/modules/order/show.blade.php
+++ resources/views/backend/modules/order/show.blade.php
@@ -44,13 +44,17 @@
                                                                             <span class="header-sm">{{ trans('general.mobile') }}</span><br/>
                                                                             {{ $element->mobile}}<br/>
                                                                             <br/>
-                                                                            <span class="header-sm">{{ trans('general.payment_method') }}</span> : {{ $element->payment_method }}
+                                                                            <span class="header-sm">{{ trans('general.payment_method') }}</span>
+                                                                            : {{ $element->payment_method }}
                                                                             <br/>
-                                                                            <span class="header-sm">{{ trans('general.price') }}</span> :{{ $element->price }}
+                                                                            <span class="header-sm">{{ trans('general.price') }}</span>
+                                                                            :{{ $element->price }}
                                                                             <br/>
-                                                                            <span class="header-sm">{{ trans('general.discount') }} : {{ $element->discount }}</span>
+                                                                            <span class="header-sm">{{ trans('general.discount') }}
+                                                                                : {{ $element->discount }}</span>
                                                                             <br/>
-                                                                            <span class="header-sm">{{ trans('general.net_price') }}</span> : {{ $element->net_price }}
+                                                                            <span class="header-sm">{{ trans('general.net_price') }}</span>
+                                                                            : {{ $element->net_price }}
                                                                         </td>
                                                                     </tr>
                                                                 </table>
@@ -99,62 +103,60 @@
                     </td>
                 </tr>
             </table>
-            <div class="col-lg-12">
-                <td class="table-content table-responsive">
-                    <table id="dataTable" class="table table-striped table-bordered table-hover" cellspacing="0">
-                        <thead>
-                        <tr>
-                            <th class="product-thumbnail hidden">{{ trans('cart.image') }}</th>
-                            <th class="product-thumbnail">{{ trans('cart.color') }}</th>
-                            <th class="product-thumbnail">{{ trans('cart.size') }}</th>
-                            <th class="product-name">{{ trans('cart.product_name') }}</th>
-                            <th class="real-product-price">{{ trans('cart.unit_price') }}</th>
-                            <th class="product-quantity">{{ trans('cart.qty') }}</th>
-                            <th class="product-subtotal">{{ trans('cart.sub_total') }}</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-
-                        @foreach($element->order_metas as $item)
-                            @if($item->product)
-                                <tr>
-                                    <td class="product-thumbnail hidden">
-                                        <a href="{{ route('frontend.product.show',$item->product_id) }}">
-                                            <img src="{{ asset('storage/uploads/images/thumbnail/'.$item->product->image) }}"
-                                                 width="100" height="100" alt="" class="img-rounded img-thumbnail"/>
-                                        </a>
-                                    </td>
-
-                                    <td class="product-thumbnail">{{ $item->product_attribute->colorName }}</td>
-                                    </td>
-
-                                    <td class="product-thumbnail">
-                                        {{ $item->product_attribute->sizeName }}
-                                    </td>
-
-                                    <td class="product-name">
-                                        <a href="{{ route('frontend.product.show',$item->product_id) }}">{{ $item->product->name }}</a>
-                                    </td>
-
-                                    <td class="real-product-price">
-                                        <span class="price">{{$item->price}} {{ trans('general.kd') }} </span>
-                                        {{--<span class="amounte">{{ $item->options->product->sale_price }} KD </span>--}}
-                                    </td>
-
-                                    <td class="product-quantity">
-                                        {{--<input type="number" name="quantity_{{$item->options->product->id}}" disabled="disabled"--}}
-                                        {{--value="{{ $item->qty }}"/>--}}
-                                        {{ $item->qty }}
-                                    </td>
-                                    <td class="product-subtotal">{{ number_format($item->price * $item->qty,'2','.',',') }} {{ trans('general.kd') }}</td>
-                                </tr>
-                            @endif
-                        @endforeach
-                        </tbody>
-                    </table>
-            </div>
         </div>
     </div>
+    <div class="col-lg-12">
+        <td class="table-content table-responsive">
+            <table id="dataTable" class="table table-striped table-bordered table-hover" cellspacing="0">
+                <thead>
+                <tr>
+                    <th class="product-thumbnail hidden">{{ trans('cart.image') }}</th>
+                    <th class="product-thumbnail">{{ trans('cart.color') }}</th>
+                    <th class="product-thumbnail">{{ trans('cart.size') }}</th>
+                    <th class="product-name">{{ trans('cart.product_name') }}</th>
+                    <th class="real-product-price">{{ trans('cart.unit_price') }}</th>
+                    <th class="product-quantity">{{ trans('cart.qty') }}</th>
+                    <th class="product-subtotal">{{ trans('cart.sub_total') }}</th>
+                </tr>
+                </thead>
+                <tbody>
 
+                @foreach($element->order_metas as $item)
+                    @if($item->product)
+                        <tr>
+                            <td class="product-thumbnail hidden">
+                                <a href="{{ route('frontend.product.show',$item->product_id) }}">
+                                    <img src="{{ asset('storage/uploads/images/thumbnail/'.$item->product->image) }}"
+                                         width="100" height="100" alt="" class="img-rounded img-thumbnail"/>
+                                </a>
+                            </td>
+
+                            <td class="product-thumbnail">{{ $item->product_attribute->colorName }}</td>
+                            </td>
+
+                            <td class="product-thumbnail">
+                                {{ $item->product_attribute->sizeName }}
+                            </td>
+
+                            <td class="product-name">
+                                <a href="{{ route('frontend.product.show',$item->product_id) }}">{{ $item->product->name }}</a>
+                            </td>
+
+                            <td class="real-product-price">
+                                <span class="price">{{$item->price}} {{ trans('general.kd') }} </span>
+                                {{--<span class="amounte">{{ $item->options->product->sale_price }} KD </span>--}}
+                            </td>
+
+                            <td class="product-quantity">
+                                {{--<input type="number" name="quantity_{{$item->options->product->id}}" disabled="disabled"--}}
+                                {{--value="{{ $item->qty }}"/>--}}
+                                {{ $item->qty }}
+                            </td>
+                            <td class="product-subtotal">{{ number_format($item->price * $item->qty,'2','.',',') }} {{ trans('general.kd') }}</td>
+                        </tr>
+                    @endif
+                @endforeach
+                </tbody>
+            </table>
     </div>
 @endsection
